01807526334e6e0d47da508da61a1879617a5e9f
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-09-21 01:55:46 +0100

    Retoques finais

--- assets/js/perguntas.js
+++ assets/js/perguntas.js
@@ -1,15 +1,18 @@
-//Perguntas
+
 let pergunta = [
     "Quem desenvolveu este Quiz?",
-    "Quais linguagens são utilizadas para front-end?"
+    "Quais linguagens são utilizadas para front-end?",
+    "Qual é a linguagem que foi utilizada para desenvolver este quiz?"
 ];
 
 let alternativas = [
     ["Eugénio Cachiombo","Joseph Dias", "Génio Cach", "Bernardo Mutangui"],
-    ["HTML, CSS, TypeScript", "Java, CSS, HTML", "Dart, C#, HTML", "HTML, CSS, JavaScript"]
+    ["HTML, CSS, TypeScript", "Java, CSS, HTML", "Dart, C#, HTML", "HTML, CSS, JavaScript"],
+    ["Python", "Java", "JavaScript", "C#"]
 ];
 
 let correcta = [
     "Eugénio Cachiombo",
-    "HTML, CSS, JavaScript"
+    "HTML, CSS, JavaScript",
+    "JavaScript"
 ];
