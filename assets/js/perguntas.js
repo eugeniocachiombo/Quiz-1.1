160dc56f296fa9c80238ab92d5053b470a9b52fd
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-11-04 13:56:26 +0100

    Perguntas modificadas

--- assets/js/perguntas.js
+++ assets/js/perguntas.js
@@ -1,21 +1,43 @@
-
 let pergunta = [
-    "Quem desenvolveu este Quiz?",
-    "Quais linguagens são utilizadas para front-end?",
-    "Qual é a linguagem que foi utilizada para desenvolver este quiz?",
-    "Quem é o Rui?",
+    "Quantos filhos teve o bisavó Francisco?",
+    "Qual é o nome verdadeiro do bisavó Francisco?",
+    "Em que cidade nascéu o patriarca F.C.R?",
+    "Qual é o nome verdadeiro da matriarca Maria?",
+    "Quantos filhos tem a Maria Raimundo?",
+    "Quantas meninas tem nos filhos da dona Maria Raimundo?",
+    "Qual é o primeiro casal de netos da dona Maria?",
+    "Quem diz esse é um sujo?",
+    "Quem é o primeiro filho da dona Maria?",
+    "Quem faz as pessoas rirem?",
+    "Quem é o Engenheiro Informático da família?",
 ];
+
 
 let alternativas = [
-    ["Eugénio Cachiombo","Joseph Dias", "Génio Cach", "Bernardo Mutangui"],
-    ["HTML, CSS, TypeScript", "Java, CSS, HTML", "Dart, C#, HTML", "HTML, CSS, JavaScript"],
-    ["Python", "Java", "JavaScript", "C#"],
-    ["Programador", "Aluno", "Estudante", "Discípulo de Eugénio"],
+    ["18","13", "22", "15"],
+    ["Francisco Cosme de Sã Raimundo", "Francisco Cosme Raimundo", "Francisco Raimundo Cosme", "Cosme Francisco Raimundo"],
+    ["Kwanza-Sul", "Lunda-Sul", "Huambo", "Bié"],
+    ["Maria da Cruz Raimundo", "Maria Quintinho Raimundo", "Maria da Concenção Raimundo", "Maria Raimundo"],
+    ["17","8", "11", "7"],
+    ["5","3", "7", "1"],
+    ["Maricéu e Rui","Otávio e Hiduiny", "Jaliane e Otávio", "Rui e Jaliane"],
+    ["PM","Pai da Hiduiny", "Damásio Sobrinho", "Sinagoga"],
+    ["Anselmo Sobrinho","Antónia Bonifácio", "Márcia Florência", "Dainy Ginga"],
+    ["Dainy Ginga","Aristóteles", "Tio Mingo", "Ri Comigo"],
+    ["Rui Vissapa","Tílha Mambo", "Maricéu Bonifácio", "Ennys Donatello"],
 ];
 
+
 let correcta = [
-    "Eugénio Cachiombo",
-    "HTML, CSS, JavaScript",
-    "JavaScript",
-    "Discípulo de Eugénio"
+    "15",
+    "Francisco Cosme Raimundo",
+    "Lunda-Sul",
+    "Maria da Concenção Raimundo",
+    "7",
+    "5",
+    "Maricéu e Rui",
+    "Damásio Sobrinho",
+    "Márcia Florência",
+    "Dainy Ginga",
+    "Rui Vissapa",
 ];
